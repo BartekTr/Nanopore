6dc1af13cde5fd193a3354d016fe4f5a2cbd1b93
Piotr <piotr.samborski007@gmail.com>
2020-06-11 19:51:29 +0200

    .

--- Nanopore/kernel.cu
+++ Nanopore/kernel.cu
@@ -4,8 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+
+#include<bitset>
+#include<list>
 //code from https://stackoverflow.com/questions/735126/are-there-alternate-implementations-of-gnu-getline-interface/735472#735472
+
+#define K 5
+
 typedef intptr_t ssize_t;
+typedef std::bitset<4> BYTE;
+
+typedef struct K_MER_NODE
+{
+    BYTE K_MER[K];
+    short K_MER_QUALITY;
+
+} K_MER_NODE;
 
 ssize_t getline(char** lineptr, size_t* n, FILE* stream) {
     size_t pos;
@@ -54,21 +68,83 @@ ssize_t getline(char** lineptr, size_t* n, FILE* stream) {
     (*lineptr)[pos] = '\0';
     return pos;
 }
+
+BYTE char_to_byte(char c)
+{
+    BYTE byte;
+    if (c == 'A')
+        byte = 8;
+    else if (c == 'C')
+        byte = 4;
+    else if (c == 'T')
+        byte = 2;
+    else if (c == 'G')
+        byte = 1;
+    else
+        byte = 0;
+
+    return byte;
+}
+
 int main()
 {
-    char* buf = NULL;   
+    std::list<K_MER_NODE> K_MER_NODE_LIST = {};
+
+    char* buf = NULL;  
+    char* genotype = NULL;
+    char* probability = NULL;
     size_t bufSize = 10;
     FILE* f;
     size_t line_size;
-    f = fopen("D:/Pobrane_D/chr1.fastq", "r");   
+    f = fopen("G:/chr2.fastq", "r"); 
+
     int i = 0;
     do
     {
         i++;
         line_size = getline(&buf, &bufSize, f);
-        if(i%4 == 0)
-            printf("%s\n", buf);
+        if (i % 4 == 2) // Set A, C, T, G as BYTE
+        {
+            int length = strlen(buf);
+            if (genotype != NULL)
+            {
+                free(genotype);
+            }
+            genotype = (char*)malloc(length * sizeof(char));
+            memcpy(genotype, buf, length);
+        }
+        else if (i % 4 == 0) // Set probability
+        {
+            probability = buf;
+        }
 
+        if (i % 4 == 0)
+        {
+            int length = strlen(genotype);
+            for (int x = 0; x < length - K; x++)
+            {
+                K_MER_NODE k_mer;
+                long prob = 0;
+                for (int k_len = 0; k_len < K; k_len++)
+                {
+                    k_mer.K_MER[k_len] = char_to_byte(genotype[x + k_len]);
+                    prob += (int)probability[x + k_len];
+                }
+
+                k_mer.K_MER_QUALITY = prob / length;
+                K_MER_NODE_LIST.push_back(k_mer);
+                //printf("%c\n", genotype[x]);
+                //printf("%c\n", probability[x]);
+                //printf("\n");
+                //char char_to_byte
+                
+            }
+            printf("K_MER");
+        }
+
+        //printf("4 lines!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            //if (i == 4)
+              //  break;
     } while (line_size > 0);
 
 
